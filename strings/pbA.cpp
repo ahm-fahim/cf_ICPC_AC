b75114f7434e0a67dad547f595cdd81e463c712e
ahm-fahim <contactahmfahim@gmail.com>
2023-08-16 21:59:08 +0600

    solved

---
+++ strings/pbA.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S, T;
+    cin >> S ;
+    cin >> T;
+
+    cout << S.length() << " " << T.length() << "\n";
+    cout << S << " " << T << "\n";
+
+    return 0;
+}
