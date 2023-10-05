15500a8405d2c6cd97d1d730eccc0cc26b290881
ahm-fahim <contactahmfahim@gmail.com>
2023-10-05 20:26:32 +0600

    stl contest

---
+++ sheet#1(data_type-conditon)/K.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin >> a >>b>>c;
+    int mx = 0;
+    int mn= 0;
+
+
+    if(a>b && a > c){
+        mx = a;
+    }else if(b>)
+
+
+}
